89204f4d5bc57e366de4f29269dbc8f3aaad1e24
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-04-19 20:01:19 -0300

    a

--- src/app/comentario/comentario.component.ts
+++ src/app/comentario/comentario.component.ts
@@ -9,15 +9,15 @@ import { Component } from '@angular/core';
 })
 export class ComentarioComponent {
   textExpanded: boolean = false;
-  textBoxHeight: string = '150px';
+  textBoxHeight: string = '170px';
 
   toggleText() {
     this.textExpanded = !this.textExpanded;
     if (this.textExpanded) {
-      this.textBoxHeight = '200px';
+      this.textBoxHeight = 'auto';
 
     } else {
-      this.textBoxHeight = '200px';
+      this.textBoxHeight = '150px';
 
     }
   }
